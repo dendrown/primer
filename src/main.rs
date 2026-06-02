1d94b64c638ea269ebdff9b329f7411dfe685c90
den Drown <den@dendrown.net>
2026-06-02 11:34:41 -0400

    Base code to accept a prime candidate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let arg = env::args().nth(1).expect("Usage: primer <number>");
+    let n: u64 = arg.parse().expect("Invalid whole number");
+
+    println!("PRIME? {:?}", n);
 }
