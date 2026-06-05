09eb77ea6cff81a81c3e5697406064922f423136
den Drown <den@dendrown.net>
2026-06-05 16:40:24 -0400

    Pull all integer primes in a store

--- src/store.rs
+++ src/store.rs
@@ -2,7 +2,8 @@
 /// Persistent storage of Integer and Big Integer prime numbers
 ///
 /// Copyright (c) 2026 Dennis Drown
-//use std::fs;
+use std::fs;
+use std::io::{self, BufReader, Read};
 
 const STORE_U64: &str = "primes-u64.dat";
 const STORE_BIG: &str = "primes-big.dat";
@@ -19,5 +20,29 @@ impl Default for Store {
 
     fn default() -> Self {
         Store {int: STORE_U64, big: STORE_BIG}
+    }
+}
+
+
+impl Store {
+    pub fn read_all_int(self) -> Result<Vec<u64>, io::Error> {
+    //! Read all prime numbers currently in the Integer store
+    //! TODO: Only read up to a requested value for n
+        let /*mut*/ store = fs::OpenOptions::new()
+            .read(true)
+            //.append(true)
+            .open(self.int)?;
+        let mut reader = BufReader::new(store);
+        let mut buffer = [0u8; 8];
+        let mut primes = Vec::<u64>::new();
+
+        loop {
+            match reader.read_exact(&mut buffer) {
+                Ok(()) => primes.push(u64::from_ne_bytes(buffer)),
+                Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => break,
+                Err(e) => return Err(e),
+            }
+        }
+        Ok(primes)
     }
 }
